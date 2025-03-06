8b231c7e544e3f67d685572a284ee99ad469e50c
Nikita Babaev <maximrossa1@gmail.com>
2025-03-06 18:58:01 +0400

    Extended BAD_WORDS list

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,16 @@
-INTERNSHIP_LIST_PATH = 'publications'
+INTERNSHIP_LIST_PATH = 'https://fut.ru/api/cms/api/publications'
+EVENTS_LIST_PATH = 'https://fut.ru/api/forms/api/events'
 
 INDEX_NAME = 'internships'
 
-BAD_WORDS = {'&nbsp;', '<br>', '</b>', '&amp; '}
+BAD_WORDS = {
+    '&nbsp;',
+    '<br>',
+    '</b>',
+    '<b>',
+    '&amp; ',
+    '<i>',
+    '</i>',
+    '<a>',
+    '</a>',
+}
